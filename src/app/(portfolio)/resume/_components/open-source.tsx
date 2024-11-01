83ddf7644887bb6577158964cf68f54baa5cff2f
Programmer-Himanshu-Arya <aryah144@gmail.com>
2024-11-02 00:40:06 +0530

    fifth commit

--- src/app/(portfolio)/resume/_components/open-source.tsx
+++ src/app/(portfolio)/resume/_components/open-source.tsx
@@ -4,18 +4,18 @@ import { A, H3, H4 } from '~/components/typography';
 import json from '../../../../../data/github-data.json';
 
 export async function OpenSource() {
-  const data = json.data;
+  // const data = json.data;
   function getLastMonthCommits() {
-    return data.user.contributionsCollection.contributionCalendar.weeks
-      .slice(-4)
-      .reduce((acc, week) => {
-        return (
-          acc +
-          week.contributionDays.reduce((acc, day) => {
-            return acc + day.contributionCount;
-          }, 0)
-        );
-      }, 0);
+    // return data.user.contributionsCollection.contributionCalendar.weeks
+    //   .slice(-4)
+    //   .reduce((acc, week) => {
+    //     return (
+    //       acc +
+    //       week.contributionDays.reduce((acc, day) => {
+    //         return acc + day.contributionCount;
+    //       }, 0)
+    //     );
+    //   }, 0);
   }
   return (
     <section>
@@ -23,16 +23,16 @@ export async function OpenSource() {
         <tbody>
           <tr>
             <td className="text-muted-foreground md:pr-4">Total Repositories:</td>
-            <td>Contributed to {data.user.repositoriesContributedTo.totalCount} Repos</td>
+            <td>Contributed to 13 Repos</td>
           </tr>
           <tr>
             <td className="text-muted-foreground md:pr-4">Top Repository:</td>
-            <td>{data.user.topRepositories.nodes[0].nameWithOwner}</td>
+            <td>hello/hello</td>
           </tr>
           <tr>
             <td className="text-muted-foreground md:pr-4">PRs Merged:</td>
             <td className="flex items-center">
-              {data.user.pullRequests.totalCount}
+              test
               <GitPullRequest className="ml-3 mr-1 inline-block size-4 text-orange-200 md:size-5" />
               Pull Requests
             </td>
@@ -40,22 +40,21 @@ export async function OpenSource() {
           <tr>
             <td className="text-muted-foreground md:pr-4">Issues Open:</td>
             <td className="flex items-center">
-              {data.user.issues.totalCount}
+              test
               <CircleDot className="ml-3 mr-1 inline-block size-4 text-orange-200 md:size-5" />
               Issues
             </td>
           </tr>
         </tbody>
       </table>
-      <A href={data.user.url}>github.com/zaCKoZAck0</A>
       <H3 className="text-sm text-muted-foreground md:text-2xl">
-        {getLastMonthCommits()} Github Contributions in the last Month
+        2 Github Contributions in the last Month
       </H3>
       <H4 className="mb-1 text-xs text-muted-foreground md:text-xl">
-        {data.user.contributionsCollection.contributionCalendar.totalContributions} Github
+       90 Github
         Contributions in the last Year (365 Days)
       </H4>
-      <CommitGraph contributions={data.user.contributionsCollection.contributionCalendar.weeks} />
+      {/* <CommitGraph contributions={data.user.contributionsCollection.contributionCalendar.weeks} /> */}
     </section>
   );
 }
