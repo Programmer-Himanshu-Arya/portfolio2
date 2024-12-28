ec1180c1124bdf1426a1a4d95162ebf4c43ddec6
Himanshu Arya <contact.himanshu.arya@gmail.com>
2024-12-28 18:02:36 +0530

    Update footer.tsx

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <div className="w-full pb-2 pt-5 text-xs">
       <div className="flex flex-grow flex-col justify-between gap-2 rounded-md bg-muted/50 px-2 py-1 text-muted-foreground md:px-4 md:py-2">
         <div className="flex justify-between">
-          <p className="flex gap-2">Made with ❤️ by Himanshu Arya</p>
+          <p className="flex gap-2">AryaDevs</p>
           <p className="flex gap-5 text-xl">
             <a href="https://github.com/CodeDevArya" target="_blank">
               <FaGithub />
